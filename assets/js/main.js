605114cb7efa04e33774c793162075836d916878
PedroZangirolami <pedrozangirolami@gmail.com>
2023-09-06 14:36:15 -0300

    criacao da model de detalhe do pokemon

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
+const modalMensagem = document.getElementById('modal-mensagem')
 
 const maxRecords = 151
 const limit = 10
@@ -7,19 +8,21 @@ let offset = 0;
 
 function convertPokemonToLi(pokemon) {
     return `
-        <li class="pokemon ${pokemon.type}">
-            <span class="number">#${pokemon.number}</span>
-            <span class="name">${pokemon.name}</span>
+        <div id="clicavel" onclick="executaAcao(${pokemon.number})">
+            <li class="pokemon ${pokemon.type}">
+                <span class="number">#${pokemon.number}</span>
+                <span class="name">${pokemon.name}</span>
 
-            <div class="detail">
-                <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                </ol>
+                <div class="detail">
+                    <ol class="types">
+                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                    </ol>
 
-                <img src="${pokemon.photo}"
-                     alt="${pokemon.name}">
-            </div>
-        </li>
+                    <img src="${pokemon.photo}"
+                        alt="${pokemon.name}">
+                </div>
+            </li>
+        </div>
     `
 }
 
@@ -45,3 +48,31 @@ loadMoreButton.addEventListener('click', () => {
         loadPokemonItens(offset, limit)
     }
 })
+
+function createModal() {
+    return `    
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal"><span>×</span></button>
+                    <h4 class="modal-title">Título da mensagem</h4>
+                </div>
+                <div class="modal-body">
+                    <p>Conteúdo da mensagem</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>
+                </div>
+            </div>
+        </div>`
+}
+
+function executaAcao(pokemonId){
+    
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        //const newHtml = pokemons.map(convertPokemonToLi).join('')
+        modalMensagem.innerHTML = createModal()
+    })
+
+    $("#modal-mensagem").modal();
+}
